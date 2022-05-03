944f04cf3a25ce819b73d02a8f395dc1db0fd8a8
willian-virgilio <willian.virgilio@gmail.com>
2022-05-02 22:43:40 -0300

    arvore rotacionando e lendo elementos do arquivo entrada.txt, trabalhar na saida.txt, versão de impressão

--- arqvIO.py
+++ arqvIO.py
@@ -33,7 +33,7 @@ class Arquivo:
         for i in range(len(vetor)):
             # O primeiro elemento da lista separador é o comando, o segundo é o valor lista_comandos_e_valores = [0]
             lista_comandos_e_valores = vetor[i].split()
-            print("Tamanho da lista apos split: ", len(lista_comandos_e_valores))
+           # print("Tamanho da lista apos split: ", len(lista_comandos_e_valores))
 
             # primeiro if, caso o vetor1addr tenha indices maior que 2, então ele é o comando SUC de sucessor
             if( len(lista_comandos_e_valores) > 2):
@@ -45,24 +45,27 @@ class Arquivo:
                     arqv_saida.write(a + ' ')
             # verificar erro AttributeError: 'str' object has no attribute 'loopDeGravacao'
 
-            elif(lista_comandos_e_valores[0] == 'INC'):
+            else:
+                if(lista_comandos_e_valores[0] == 'INC'):
                 #print("O comando é :", lista_comandos_e_valores[0])
                 # print("O elemento a ser INCLUIDO é: ", lista_comandos_e_valores[1])
                     nova.inserirNovoNo(lista_comandos_e_valores[1])
                     nova.mostrar_arvore()
 
-            elif(lista_comandos_e_valores[0] == 'REM'):
+
+                if(lista_comandos_e_valores[0] == 'REM'):
                 # print("O comando é :", lista_comandos_e_valores[0])
                 # print("O elemento a ser REMOVIDO é: ", lista_comandos_e_valores[1])
                     nova.deletarNo(lista_comandos_e_valores[1])
                     print("\n ----- Depois deletar o elemento: ",lista_comandos_e_valores[1])
                     nova.mostrar_arvore()
-            elif(lista_comandos_e_valores[0] == 'IMP'):
+
+                if(lista_comandos_e_valores[0] == 'IMP'):
                 # print("O comando é :", lista_comandos_e_valores[0])
                 #  print("O elemento a ser INCLUID é: ", lista_comandos_e_valores[1],end=' ' )
-                for i in range(len(lista_comandos_e_valores)):
-                    a = lista_comandos_e_valores[i]
-                    arqv_saida.write(a + ' ')
+                    for i in range(len(lista_comandos_e_valores)):
+                        a = lista_comandos_e_valores[i]
+                        arqv_saida.write(a + ' ')
 
             arqv_teste.write(vetor[i]+' ')
         arqv_teste.close()
