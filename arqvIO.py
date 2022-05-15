03307534e2118bec836b5a4f1ee88422eb3311da
willian-virgilio <willian.virgilio@gmail.com>
2022-05-15 15:33:50 -0300

    fuNCIONANDO É MELHORANDO, AGORA FALTA ALTERAR OS ALERTAS CASO SUC SEJA MAIOR E IMPRESSÃO TAMBEM

--- arqvIO.py
+++ arqvIO.py
@@ -19,6 +19,10 @@ class Arquivo:
     def __init__(self,nomeArqv):
         self.nomeArqv = nomeArqv
         self.smsErro = False
+        self.concatenar_antes = ''
+        self.concatenar_apos = ''
+        self.concatenar_no01_nivel01 = ''
+        self.stringConcatenadaSUC = ''
 
 
     def getStringRaizCentro(self):
@@ -45,7 +49,8 @@ class Arquivo:
 
         self.smsErro = 'V'
 
-
+    def get_stringConcate(self):
+        return self.stringConcatenadaSUC
 
     def gravarCorrigido(self,corrigido,index,elemento):
         arqv_saida = open('saida.txt', 'a')
@@ -59,7 +64,7 @@ class Arquivo:
             arqv_saida.write('\n')
 
 
-    def testeEscrita(self,e_sucessor = False):
+    def testeEscrita(self,e_sucessor=False,num_versao=0):
 #        print(self.controle_versao)
         msg_erro_index = "Erro : Valor a ser inserido ou removido é inexistente, \n" \
                          "ou a operação  contem algum caracter invalido" \
@@ -68,21 +73,35 @@ class Arquivo:
                         " 1 - Se alguma instrução esta faltando numero posterior a ela. \n" \
                         "Exemplo: INC  <-----está sem o numero da versão!!\n" \
                         "    2 - Se contem caracter invalido, não numerico.: Exemplo REM x \n"
-        print("Tamanho do vetor1: ",len(vetor))
-
-        if(e_sucessor == False):
-
+        if (e_sucessor == True):
+            vetortemp = []
+            vetortemp.append('IMP')
+            vetortemp.append(str(num_versao))
+            tam_vet = len(vetortemp)
+            print("Valor da lista tam_vetor", vetortemp)
+        else:
+            print("Tamanho do vetor1: ",len(vetor))
+            tam_vet = len(vetor)
             arqv_saida = open('saida.txt', 'a')
             arqv_saida.truncate(0)
 
 
+        lista_comandos_e_valores = []
+        lista_comandos_e_valores_SUC = []
 
-        for i in range(0,len(vetor),1):
+
+
+        for i in range(0,tam_vet,1):
+
 
             # O primeiro elemento da lista separador é o comando, o segundo é o valor lista_comandos_e_valores = [0]
-            lista_comandos_e_valores = vetor[i].split()
-            print("toda a lista :",lista_comandos_e_valores)
-            print("Tamanho da lista apos split: ", len(lista_comandos_e_valores))
+            if(e_sucessor == True):
+                lista_comandos_e_valores = vetortemp
+                print("Valor da lista, caso e_sucessor = True:",lista_comandos_e_valores)
+            else:
+                lista_comandos_e_valores = vetor[i].split()
+                print("toda a lista :",lista_comandos_e_valores)
+                print("Tamanho da lista apos split: ", len(lista_comandos_e_valores))
 
             # primeiro if, caso o vetor1addr tenha indices maior que 2, então ele é o comando SUC de sucessor
             if( len(lista_comandos_e_valores) > 2):
@@ -123,12 +142,15 @@ class Arquivo:
                 # if(len(a) == 0)
                 print('Vetor A:',a)
                 for i in range(0,len(a),4):
+                    x = int(lista_comandos_e_valores[1])
+                    y = int(a[i])
 
-                    if(int(lista_comandos_e_valores[1]) < int(a[i]) ):
+                    if( x < y ):
 
+                        sucessor.append(y)
 
-                        sucessor.append(int(a[i]))
-#
+                        zy = self.testeEscrita(True, 11)
+                        print("valor apos executar testeEscrita em SUC:", zy)
 
                     print("lista de valores de sucessores:", sucessor)
                     print("tamanho", len(sucessor))
@@ -148,6 +170,7 @@ class Arquivo:
             # verificar erro AttributeError: 'str' object has no attribute 'loopDeGravacao'
 
             else:
+
                 if(lista_comandos_e_valores[0] == 'INC'):
                 #print("O comando é :", lista_comandos_e_valores[0])
                 # print("O elemento a ser INCLUIDO é: ", lista_comandos_e_valores[1])
@@ -211,10 +234,11 @@ class Arquivo:
                   #  self.controle_versao += self.controle_versao
 
                 if (lista_comandos_e_valores[0] == 'IMP'):
-                    concatenar_raiz = ''
-                    concatenar_antes = ''
-                    concatenar_apos = ''
-                    concatenar_no01_nivel01 = ''
+                    self.concatenar_raiz = ''
+                    self.concatenar_antes = ''
+                    self.concatenar_apos = ''
+                    self.concatenar_no01_nivel01 = ''
+                    self.concatenar_raiz = ''
 
 
 
@@ -223,130 +247,142 @@ class Arquivo:
 
                     # for i in range(len(lista_comandos_e_valores)):
                     print(type(lista_comandos_e_valores[1]))
-                    if (e_sucessor == False):
-                        arqv_saida.write('IMP ')
+
 
 
 
 
                     if lista_comandos_e_valores[1] == '0' :
-                        msg = "O valor da versão solicitada para impressão IMP é 0," \
-                              "\n a primeira versão começa com valor inteiro 1 \n" \
-                              "-----------------------------------------------------"
-                        nova.gerar_log(msg,'IMP','','0')
-                    #    arqv_saida.write(lista_comandos_e_valores[1])
-                        if (e_sucessor == False):
-                            arqv_saida.write('\n')
-                        exit()
+                            msg = "O valor da versão solicitada para impressão IMP é 0," \
+                                  "\n a primeira versão começa com valor inteiro 1 \n" \
+                                  "-----------------------------------------------------"
+                            nova.gerar_log(msg,'IMP','','0')
+                        #    arqv_saida.write(lista_comandos_e_valores[1])
+                            if (e_sucessor == False):
+                                arqv_saida.write('\n')
+                                exit()
+                            else:
+                                exit()
                     else:
-                        corrigido = nova.retornar_valor_corrigido()
+                            corrigido = nova.retornar_valor_corrigido()
 
 
-                        if (int(corrigido) < int(lista_comandos_e_valores[1])):
-                            if (e_sucessor == False):
-                                arqv_saida.write(str(corrigido) + ' ')
-                                arqv_saida.write('\n')
-                            a = nova.retornar_versao(corrigido, False,'')  # valor negativo de IMP não é possivel
-                        else:
-                            if (e_sucessor == False):
-                                arqv_saida.write(lista_comandos_e_valores[1] + ' ')
-                                arqv_saida.write('\n')
-                            a = nova.retornar_versao((int(lista_comandos_e_valores[1])), False,'')  # valor negativo de IMP não é possivel
-                            controle = True
+                            if (int(corrigido) < int(lista_comandos_e_valores[1])):
+                                if (e_sucessor == False):
+                                    arqv_saida.write('IMP ')
+                                    arqv_saida.write(str(corrigido) + ' ')
+                                    arqv_saida.write('\n')
+
+                                a = nova.retornar_versao(corrigido, False,'')  # valor negativo de IMP não é possivel
+                            else:
+                                if (e_sucessor == False):
+                                    arqv_saida.write('IMP ')
+                                    arqv_saida.write(lista_comandos_e_valores[1] + ' ')
+                                    arqv_saida.write('\n')
+                                a = nova.retornar_versao((int(lista_comandos_e_valores[1])), False,'')  # valor negativo de IMP não é possivel
+                                controle = True
 
 
 
 
 
-                        tamanho_vetor_a = len(a)
-                        print("Tamanho vetor a antes do for:", tamanho_vetor_a)
+                            tamanho_vetor_a = len(a)
+                            print("Tamanho vetor a antes do for:", tamanho_vetor_a)
 
-                        concatenar_raiz = str(a[0]) + ','
-                        concatenar_raiz += str(a[1]) + ','
-                        concatenar_raiz += a[2] + ','
-                        print("Esta é a raiz: ", concatenar_raiz)
-                        if(tamanho_vetor_a <= 4):
-                            if (e_sucessor == False):
-                                arqv_saida.write(concatenar_raiz)
-                                print("Valor concaternado da stringConcatenada\n"
-                                      "se o vetor for menor que 4 : %s" % concatenar_raiz)
+                            self.concatenar_raiz = str(a[0]) + ','
+                            self.concatenar_raiz += str(a[1]) + ','
+                            self.concatenar_raiz += a[2] + ','
+                            print("Esta é a raiz: ", self.concatenar_raiz)
+                            if(tamanho_vetor_a <= 4):
+                                if (e_sucessor == False):
+                                    arqv_saida.write(self.concatenar_raiz)
 
-                            stringConcatenada = concatenar_raiz
-                            print("Valor concaternado da stringConcatenada\n"
-                                  "se o vetor for menor que 4 : %s" % stringConcatenada)
-
-                        else:
-                            concatenar_antes += str(a[4]) + ','
-                            concatenar_antes += str(a[5]) + ','
-                            concatenar_antes += a[6] + ','
-                            print("Este é o primeiro nó ", concatenar_antes)
-
-      #                          if (lista_comandos_e_valores[1] == '1'):
-       #                             arqv_saida.write(concatenar_raiz)
-        #                            print("controle antes de 1:", controle)
-         #                           controle = False
-          #                          print("valor controle depois 1: ",controle)
+                                else:
+                                    self.stringConcatenadaSUC = self.concatenar_raiz
+                                    print("Valor concaternado da self.stringConcatenadaSUC\n"
+                                      "se o vetor for menor que 4 : %s" % self.stringConcatenadaSUC)
+                                    return self.get_stringConcate()
 
 
-                          #      if lista_comandos_e_valores[1] == '2':
-                           #         arqv_saida.write(concatenar_antes)
-                            #        arqv_saida.write(concatenar_raiz)
-                             #       print("controle antes de 2:", controle)
-                              #      controle = False
-                               #     print("valor controle depois 2: ", controle)
+                            else:
+                                self.concatenar_antes += str(a[4]) + ','
+                                self.concatenar_antes += str(a[5]) + ','
+                                self.concatenar_antes += a[6] + ','
+                                print("Este é o primeiro nó ", self.concatenar_antes)
 
-                            controle_entrada = False
+          #                          if (lista_comandos_e_valores[1] == '1'):
+           #                             arqv_saida.write(self.concatenar_raiz)
+            #                            print("controle antes de 1:", controle)
+             #                           controle = False
+              #                          print("valor controle depois 1: ",controle)
 
-                            for i in range(8,tamanho_vetor_a,4):
-                                print("Valore de i:", i)
-                                valor = str(a[i])
-                                nivel = str(a[i+1])
-                                cor = a[i+2]
 
-                                if (nivel != '1'):
-                                    if(controle_entrada == False):
-                                        concatenar_antes += valor + ','
-                                        concatenar_antes += nivel + ','
-                                        concatenar_antes += cor + ','
-                                        print("Valore concatenado antes da raiz", concatenar_antes)
-                                    ################a ser removio###########
-                                    if (controle_entrada == True):
-                                        concatenar_apos += valor + ','
-                                        concatenar_apos += nivel + ','
-                                        concatenar_apos += cor + ','
-                                        print("Esta é concaternar depois ", concatenar_apos)
-                                    #############a ser removido#################
+                              #      if lista_comandos_e_valores[1] == '2':
+                               #         arqv_saida.write(self.concatenar_antes)
+                                #        arqv_saida.write(self.concatenar_raiz)
+                                 #       print("controle antes de 2:", controle)
+                                  #      controle = False
+                                   #     print("valor controle depois 2: ", controle)
 
-                                    print("Passou do no 1 do nivel 1", controle_entrada)
-                                elif (a[i + 1] == '1') and (controle_entrada == False):
-                                    concatenar_no01_nivel01 = valor + ','
-                                    concatenar_no01_nivel01 += nivel + ','
-                                    concatenar_no01_nivel01 += cor + ','
-                                    controle_entrada = True
-                                    print("Esta é no 01 do nivel 01 ", concatenar_no01_nivel01)
+                                controle_entrada = False
 
-                        if(tamanho_vetor_a>4):
-                            if (e_sucessor == False):
+                                for i in range(8,tamanho_vetor_a,4):
+                                    print("Valore de i:", i)
+                                    valor = str(a[i])
+                                    nivel = str(a[i+1])
+                                    cor = a[i+2]
 
-                                arqv_saida.write(concatenar_antes)
-                                arqv_saida.write(concatenar_raiz)
-                                arqv_saida.write(concatenar_no01_nivel01)
-                                arqv_saida.write(concatenar_apos)
-                                print("Valor concaternado da stringConcatenada\n"
-                                      "se o vetor for maior que 4 : %s%s%s%s" % (concatenar_antes,concatenar_raiz,concatenar_no01_nivel01,concatenar_apos) )
-                            stringConcatenada = concatenar_antes
-                            stringConcatenada += concatenar_raiz
-                            stringConcatenada += concatenar_no01_nivel01
-                            stringConcatenada += concatenar_apos
-                            print("Valor concaternado da stringConcatenada\n"
-                                  "se o vetor for maior que 4 :",stringConcatenada)
+                                    if (nivel != '1'):
+                                        if(controle_entrada == False):
+                                            self.concatenar_antes += valor + ','
+                                            self.concatenar_antes += nivel + ','
+                                            self.concatenar_antes += cor + ','
+                                            print("Valore concatenado antes da raiz", self.concatenar_antes)
+                                        ################a ser removio###########
+                                        if (controle_entrada == True):
+                                            self.concatenar_apos += valor + ','
+                                            self.concatenar_apos += nivel + ','
+                                            self.concatenar_apos += cor + ','
+                                            print("Esta é concaternar depois ", self.concatenar_apos)
+                                        #############a ser removido#################
 
-                        if (e_sucessor == False):
-                            arqv_saida.write('\n')
+                                        print("Passou do no 1 do nivel 1", controle_entrada)
+                                    elif (a[i + 1] == '1') and (controle_entrada == False):
+                                        self.concatenar_no01_nivel01 = valor + ','
+                                        self.concatenar_no01_nivel01 += nivel + ','
+                                        self.concatenar_no01_nivel01 += cor + ','
+                                        controle_entrada = True
+                                        print("Esta é no 01 do nivel 01 ", self.concatenar_no01_nivel01)
+
+                            if(tamanho_vetor_a>4):
+
+                                if (e_sucessor == False):
+                                    arqv_saida.write(self.concatenar_antes)
+                                    arqv_saida.write(self.concatenar_raiz)
+                                    arqv_saida.write(self.concatenar_no01_nivel01)
+                                    arqv_saida.write(self.concatenar_apos)
+                                elif (e_sucessor == True):
+                                    self.stringConcatenadaSUC = self.concatenar_antes
+                                    self.stringConcatenadaSUC += self.concatenar_raiz
+                                    self.stringConcatenadaSUC += self.concatenar_no01_nivel01
+                                    self.stringConcatenadaSUC += self.concatenar_apos
+                                    print("Valor concaternado da self.stringConcatenadaSUC\n"
+                                          "se o vetor for maior que 4 :", self.stringConcatenadaSUC)
+                                    return self.get_stringConcate()
+
+
+
+
+
+
+                    if (e_sucessor == False):
+                        arqv_saida.write('\n')
 
 
         if (e_sucessor == False):
             arqv_saida.close()
+
+
 if __name__ == "__main__":
 
     nova = ArvRB()
@@ -356,7 +392,9 @@ if __name__ == "__main__":
 
     x = Arquivo('entrada.txt')
     x.lerArquivo()
-
-    x.testeEscrita()
+    x.testeEscrita(False,0)
+   # zy = x.testeEscrita(True, 1)
+  #  print("valor apos executar testeEscrita em SUC:")
+  #  print("valor apos executar testeEscrita em SUC:", zy)
 
 
